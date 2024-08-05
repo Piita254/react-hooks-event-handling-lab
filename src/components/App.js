6093458d6b000d7fb83090df88cb3050622ecbba
Peter Ndungu <petersonndungu995@gmail.com>
2024-08-05 18:38:49 +0300

    all

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EyesOnMe   from './EyesOnMe';
+import Keypad from './Keypad';
+
+function App() {
+    return(
+        <>
+        <EyesOnMe/>
+        <Keypad/>
+        </>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
-// Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+import React from 'react';
+
+const Keypad = () => {
+  // Event handler for input change
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input 
+        type="password" 
+        onChange={handleChange} 
+        placeholder="Enter password"
+      />
+    </div>
+  );
+};
 
 export default Keypad;
